458b481a067e92859fbefaa5e1360242480c4aac
clemente112021 <aecojym@gmail.com>
2025-04-08 03:04:56 +0000

    comentario

--- src/styles/script.js
+++ src/styles/script.js
@@ -1,24 +1,19 @@
 let currentColor = 'red';
 let colors = ['red', 'yellow', 'green'];
 
-
 function setLight(color) {
-
     document.querySelectorAll('.light').forEach(light => {
         light.classList.remove('glow');
     });
-
     document.querySelector(`.${color}`).classList.add('glow');
     currentColor = color;
 }
-
 
 function toggleColor() {
     const currentIndex = colors.indexOf(currentColor);
     const nextIndex = (currentIndex + 1) % colors.length;
     setLight(colors[nextIndex]);
 }
-
 
 function addPurpleLight() {
     if (!colors.includes('purple')) {
